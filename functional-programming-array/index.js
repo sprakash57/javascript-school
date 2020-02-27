542af413dce6b8506a5c0291cc2099d713c757eb
Sunny Prakash <sunny.prakashgm@gmail.com>
2020-02-27 22:52:01 +0530

    added functional programming code

---
+++ functional-programming-array/index.js
@@ -0,0 +1,108 @@
+// ## Array Cardio Day 1
+
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+
+// Array.prototype.map()
+// 2. Give us an array of the inventors first and last names
+
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live all together?
+
+// 5. Sort the inventors by years lived
+
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+
+// 8. Reduce Exercise
+
+const boulevards = [
+    "Boulevard des Capucines",
+    "Boulevard de la Chapelle",
+    "Boulevard de Clichy",
+    "Boulevard du Crime"
+]
+
+const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+    { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+];
+
+const people = [
+    'Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry',
+    'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter',
+    'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas',
+    'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving',
+    'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin',
+    'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine',
+    'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'
+];
+
+// Sum up the instances of each of these
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck'];
+
+class Excercise {
+
+    get wereBorn() {
+        return inventors.filter(i => i.year >= 1500 && i.year < 1600)
+    }
+
+    get fullName() {
+        return inventors.map(i => `${i.first} ${i.last}`);
+    }
+
+    get sortInventors() {
+        return inventors.sort((a, b) => b.year - a.year);
+    }
+
+    get totalYears() {
+        return inventors.reduce((a, c) => a + (c.passed - c.year), 0, 0)
+    }
+
+    get sortByLivingYears() {
+        return inventors.sort((a, b) => (a.passed - a.year) - (b.passed - b.year))
+    }
+
+    get searchBoulvards() {
+        return boulevards.filter(i => i.includes('de'))
+    }
+
+    lastName = (name) => {
+        return name.split(', ')[0];
+    }
+
+    get sortByLastName() {
+        return people.sort((a, b) => this.lastName(a) > this.lastName(b) ? 1 : -1)
+    }
+
+    get instanceSum() {
+        return data.reduce((obj, item) => {
+            if (!obj[item]) {
+                obj[item] = 0;
+            }
+            obj[item]++;
+            return obj;
+        }, {})
+
+    }
+}
+
+const ex = new Excercise();
+console.log(ex.instanceSum)
